2dc14232603c0afa7765b3100a519928562f615e
aka <aaronkaltman@gmail.com>
2024-02-23 01:24:44 -0500

    ok now I don't know

--- src/components/counter-buttons.tsx
+++ src/components/counter-buttons.tsx
@@ -4,24 +4,24 @@ import React from "react";
 export default function CounterButtons({ count, setCount }: any) {
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center">
         <button
           onClick={(e) => {
             e.preventDefault();
             setCount(count - 1);
           }}
-          className="btn"
+          className="btn px-10 py-10"
         >
-          <Minus size={40} className="" />
+          <Minus size={40} className="-mt-4" />
         </button>
         <button
           onClick={(e) => {
             e.preventDefault();
             setCount(count + 1);
           }}
-          className="btn "
+          className="btn px-10 py-10 "
         >
-          <Plus size={40} />
+          <Plus size={40} className="-mt-4" />
         </button>
       </div>
     </>
